b510878fd2d810013b103876c3203a06223b6aaa
Johab Freitas <freitasjohab@gmail.com>
2022-03-11 14:10:58 -0300

    Ajuste

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -8,7 +8,7 @@ function Navbar(){
         <nav className="container">
           <div className="dsmovie-nav-content">
             <h1>DSMovie</h1>
-            <a href="https://github.com/johabfreitas" target="_blank">
+            <a href="https://github.com/devsuperior">
               <div className="dsmovie-contact-container">
                 <GithubIcon />
                 <p className="dsmovie-contact-link">/johabfreitas</p>
